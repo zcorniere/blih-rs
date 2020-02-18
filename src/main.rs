714a389425113d3b319382654acbca9ca66397b9
Zacharie Corniere <zacharie.corniere@epitech.eu>
2020-02-18 20:12:23 +0100

    chore(main): added request

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,34 @@
 use lib_blih::Blih;
-use json::JsonValue;
-use http::{Request, Response};
+use std::collections::HashMap;
+
+use reqwest::Url;
+use reqwest::header::USER_AGENT;
 
 fn main()
 {
     let mut auth = Blih::new("/whoami".to_string(), Some("zacharie.corniere@epitech.eu".to_string()), None);
     auth.ask_password();
 
-    let mut json = JsonValue::new_object();
-    json.insert("user", auth.get_user().as_str());
-    json.insert("signature", auth.get_token().as_ref().unwrap().as_str());
-    let request = Request::get(lib_blih::URL.to_owned() + auth.get_request())
-                      .header("User-Agent", auth.get_user_agent())
-                      .header("Content-Type", "application/json")
-                      .body(json.dump()).unwrap();
+    let mut map = HashMap::new();
+    map.insert("user", auth.get_user().as_str());
+    map.insert("body", auth.get_token().as_ref().unwrap().as_str());
+
+    let mut uri = String::from(lib_blih::URL);
+    uri.push_str(auth.get_request());
+    let uri = Url::parse(uri.as_str()).unwrap();
+    let client = reqwest::blocking::Client::new();
+    let res = client.get(uri)
+                 .header(USER_AGENT, auth.get_user_agent())
+                 .json(&map).send();
+
+    let res = match res {
+        Ok(o) => o,
+        Err(e) => {
+            println!("Error : {}", e);
+            panic!();
+        },
+    };
+    println!("res");
+    println!("status = {:?}", res.status());
+    println!("body = {:?}", res.text().unwrap());
 }
