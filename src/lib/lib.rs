04b13212a3a0212946235c73b5c5bc84fbaaf4b9
Zacharie Corniere <zacharie.corniere@epitech.eu>
2020-02-19 11:16:01 +0100

    feat(whoami): added whoami request

--- src/lib/lib.rs
+++ src/lib/lib.rs
@@ -4,11 +4,20 @@ use crypto::hmac::Hmac;
 use crypto::mac::Mac;
 use rpassword::prompt_password_stdout;
 
+mod blih_err;
+use crate::blih_err::BlihErr;
+
+use std::collections::HashMap;
+use reqwest::Url;
+use reqwest::blocking::Response;
+use reqwest::header::USER_AGENT;
+
 const VERSION :&str = "1.7";
-pub const URL: &str = "https://blih.epitech.eu/";
+pub const URL: &str = "https://blih.epitech.eu";
 
 pub struct Blih {
     user_agent: String,
+    method: reqwest::Method,
     request: String,
     user: String,
     token: Option<String>,
@@ -16,7 +25,7 @@ pub struct Blih {
 
 impl Blih {
     /// return a new Blith struct
-    pub fn new(request :String, user :Option<String>, token :Option<String>) -> Blih {
+    pub fn new(request :String, user :Option<String>, token :Option<String>, method :reqwest::Method) -> Blih {
         let user = match user {
             Some(s) => s,
             None    => std::env::var("BLIH_USER").unwrap(),
@@ -31,6 +40,7 @@ impl Blih {
         let user_agent = String::from("blih-".to_owned() + VERSION);
         Blih {
             user_agent,
+            method,
             request,
             user,
             token,
@@ -43,10 +53,10 @@ impl Blih {
         match prompt_password_stdout("Password: ") {
             Ok(s) => {
                 let mut hash = Sha512::new();
-                hash.input_str(&self.user);
+                hash.input_str(&s);
                 let hex = hash.result_str();
                 let mut hmac = Hmac::new(Sha512::new(), hex.as_bytes());
-                hmac.input(s.as_bytes());
+                hmac.input(self.user.as_bytes());
                 let hex = hmac.result();
                 let hex = hex.code();
                 self.token = Some(hex::encode(hex))
@@ -73,5 +83,30 @@ impl Blih {
     /// return the token
     pub fn get_token(&self) -> &Option<String> {
         &self.token
+    }
+
+    pub fn send_request(&self) -> Result<Response, BlihErr> {
+        let mut map = HashMap::new();
+        map.insert("user", self.user.as_str());
+        map.insert("signature", match &self.token {
+            Some(s) => s.as_str(),
+            None    => return Err(BlihErr::NoTokenProvided),
+        });
+        let mut uri = String::from(URL);
+        uri.push_str(&self.request);
+        let uri = match Url::parse(uri.as_str()) {
+                Ok(o)  => o,
+                Err(_) => return Err(BlihErr::InvalidUrl),
+            };
+
+        let client = reqwest::blocking::Client::new();
+        let res = client.request(self.method.clone(), uri)
+                    .header(USER_AGENT, &self.user_agent)
+                    .json(&map).send();
+        let res = match res {
+            Ok(o)  => o,
+            Err(_) => return Err(BlihErr::RequestFailed),
+        };
+        Ok(res)
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,13 @@
 use lib_blih::Blih;
-use std::collections::HashMap;
-
-use reqwest::Url;
-use reqwest::header::USER_AGENT;
 
 fn main()
 {
-    let mut auth = Blih::new("/whoami".to_string(), Some("zacharie.corniere@epitech.eu".to_string()), None);
+    let mut auth = Blih::new("/whoami".to_string(), Some("zacharie.corniere@epitech.eu".to_string()), None, reqwest::Method::GET);
     auth.ask_password();
 
-    let mut map = HashMap::new();
-    map.insert("user", auth.get_user().as_str());
-    map.insert("body", auth.get_token().as_ref().unwrap().as_str());
-
-    let mut uri = String::from(lib_blih::URL);
-    uri.push_str(auth.get_request());
-    let uri = Url::parse(uri.as_str()).unwrap();
-    let client = reqwest::blocking::Client::new();
-    let res = client.get(uri)
-                 .header(USER_AGENT, auth.get_user_agent())
-                 .json(&map).send();
-
-    let res = match res {
+    let res = match auth.send_request() {
         Ok(o) => o,
-        Err(e) => {
-            println!("Error : {}", e);
-            panic!();
-        },
+        Err(e) => panic!("{}", e),
     };
     println!("res");
     println!("status = {:?}", res.status());
