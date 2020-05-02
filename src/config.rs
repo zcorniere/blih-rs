c6377a41653e329508bc99db4df41cc097f80369
Zacharie Corniere <zacharie.corniere@epitech.eu>
2020-05-02 12:00:30 +0200

    chore(fmt): run cargo fmt

--- src/config.rs
+++ src/config.rs
@@ -1,7 +1,7 @@
-use std::path::PathBuf;
+use json::JsonValue;
 use std::fs::OpenOptions;
 use std::io::{Read, Write};
-use json::JsonValue;
+use std::path::PathBuf;
 
 pub struct Config {
     user: Option<String>,
@@ -15,7 +15,7 @@ impl Config {
     pub fn new_empty(path: Option<PathBuf>) -> Config {
         let path = match path {
             Some(s) => s,
-            None    => PathBuf::new(),
+            None => PathBuf::new(),
         };
         Config {
             user: None,
@@ -46,12 +46,15 @@ impl Config {
         }
     }
 
-    pub fn get_config(args: Option<&str>) -> Config
-    {
+    pub fn get_config(args: Option<&str>) -> Config {
         let config_path = get_config_path(args);
-        let mut file = match OpenOptions::new().read(true).write(true).create(true)
-                         .open(&config_path) {
-            Ok(o)  => o,
+        let mut file = match OpenOptions::new()
+            .read(true)
+            .write(true)
+            .create(true)
+            .open(&config_path)
+        {
+            Ok(o) => o,
             Err(_) => return Config::new_empty(Some(config_path)),
         };
         let mut content = String::new();
@@ -96,36 +99,39 @@ impl Config {
         self.token = val.clone();
     }
 
-
     pub fn dump(&self) -> Result<Box<JsonValue>, ()> {
         let mut content = Box::new(JsonValue::new_object());
         if self.user.is_some() {
             match content.insert("user", self.user.as_ref().unwrap().as_str()) {
                 Err(_) => return Err(()),
-                _      => (),
+                _ => (),
             }
         }
         if self.token.is_some() {
             match content.insert("token", self.token.as_ref().unwrap().as_str()) {
                 Err(_) => return Err(()),
-                _      => (),
+                _ => (),
             }
         }
         if self.baseurl.is_some() {
             match content.insert("baseurl", self.baseurl.as_ref().unwrap().as_str()) {
                 Err(_) => return Err(()),
-                _      => (),
+                _ => (),
             }
         }
         Ok(content)
     }
 
-    pub fn write_config(&self)
-    {
-        if !self.changed { return; }
+    pub fn write_config(&self) {
+        if !self.changed {
+            return;
+        }
         let json = self.dump().unwrap().pretty(4);
-        let mut file = OpenOptions::new().write(true).create(true)
-                       .open(&self.file).unwrap();
+        let mut file = OpenOptions::new()
+            .write(true)
+            .create(true)
+            .open(&self.file)
+            .unwrap();
         file.write_all(json.as_bytes()).unwrap();
         file.set_len(json.as_bytes().len() as u64).unwrap();
     }
@@ -138,8 +144,7 @@ impl Config {
 /// else if the `-p` is set, use it's value
 ///
 /// else use `$HOME/.config/blih`
-fn get_config_path(args: Option<&str>) -> PathBuf
-{
+fn get_config_path(args: Option<&str>) -> PathBuf {
     let home_path = std::env::var("HOME");
     let env_path = std::env::var("BLIH_PATH");
 
@@ -147,7 +152,7 @@ fn get_config_path(args: Option<&str>) -> PathBuf
         Ok(s) => s,
         Err(_) => match args {
             Some(s) => String::from(s),
-            None    => match home_path {
+            None => match home_path {
                 Ok(s) => s,
                 Err(_) => String::from("."),
             },
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,10 @@
 extern crate blih_rs;
 extern crate json;
-#[macro_use]extern crate clap;
+#[macro_use]
+extern crate clap;
 
-#[allow(dead_code)]mod config;
+#[allow(dead_code)]
+mod config;
 use config::Config;
 
 use blih_rs::Blih;
@@ -10,8 +12,7 @@ use blih_rs::BlihErr;
 use clap::App;
 use clap::ArgMatches;
 
-fn main()
-{
+fn main() {
     let yaml = load_yaml!("cli.yml");
     let args = App::from_yaml(yaml).get_matches();
 
@@ -35,20 +36,20 @@ fn main()
     }
 
     let res = match args.subcommand_name() {
-        Some("whoami")     => auth.whoami(),
+        Some("whoami") => auth.whoami(),
         Some("repository") => repo_sub_cmd(args.subcommand_matches("repository"), &auth),
-        Some("sshkey")     => sshkey_sub_cmd(args.subcommand_matches("sshkey"), &auth),
-        _                  => {
+        Some("sshkey") => sshkey_sub_cmd(args.subcommand_matches("sshkey"), &auth),
+        _ => {
             println!("No command provided. Rerun with -h");
             return;
-            },
+        }
     };
     let res = match res {
         Ok(o) => o,
         Err(e) => panic!("Err: {}", e),
     };
     let res = match json::parse(&res) {
-        Ok(s)  => s,
+        Ok(s) => s,
         Err(s) => panic!("Malformed request : {}", s),
     };
     println!("{}", res.pretty(4));
@@ -56,50 +57,47 @@ fn main()
     config.write_config();
 }
 
-fn blih_from_config(args: &ArgMatches, config: &mut Config) -> Blih
-{
+fn blih_from_config(args: &ArgMatches, config: &mut Config) -> Blih {
     let user = match args.value_of("user") {
         Some(s) => Some(s.to_string()),
-        None    => match std::env::var("BLIH_USER") {
-            Ok(o)  => Some(o),
+        None => match std::env::var("BLIH_USER") {
+            Ok(o) => Some(o),
             Err(_) => match config.get_user() {
                 Some(s) => Some(s.to_string()),
-                None    => None,
+                None => None,
             },
         },
     };
     let token = match args.value_of("token") {
         Some(s) => Some(s.to_string()),
-        None    => match std::env::var("BLIH_TOKEN") {
-            Ok(o)  => Some(o),
+        None => match std::env::var("BLIH_TOKEN") {
+            Ok(o) => Some(o),
             Err(_) => match config.get_token() {
                 Some(s) => Some(s.to_string()),
-                None    => None,
+                None => None,
             },
         },
     };
     let baseurl = match args.value_of("baseurl") {
         Some(s) => Some(s.to_string()),
-        None    => match std::env::var("BLIH_URL") {
-            Ok(o)  => Some(o),
+        None => match std::env::var("BLIH_URL") {
+            Ok(o) => Some(o),
             Err(_) => match config.get_baseurl() {
                 Some(s) => Some(s.to_string()),
-                None    => None,
+                None => None,
             },
         },
     };
     config.set_user(&user);
     config.set_token(&token);
     config.set_baseurl(&baseurl);
-    Blih::new(
-        user.as_deref(), token.as_deref(), baseurl.as_deref())
+    Blih::new(user.as_deref(), token.as_deref(), baseurl.as_deref())
 }
 
-fn repo_sub_cmd(args: Option<&ArgMatches>, auth: &Blih) -> Result<String, BlihErr>
-{
+fn repo_sub_cmd(args: Option<&ArgMatches>, auth: &Blih) -> Result<String, BlihErr> {
     let args = match args {
         Some(s) => s,
-        None    => panic!(),
+        None => panic!(),
     };
     match args.subcommand() {
         ("list", Some(_)) => auth.list_repo(),
@@ -107,19 +105,22 @@ fn repo_sub_cmd(args: Option<&ArgMatches>, auth: &Blih) -> Result<String, BlihEr
         ("delete", Some(s)) => auth.delete_repo(s.value_of("NAME").unwrap()),
         ("create", Some(s)) => auth.create_repo(s.value_of("NAME").unwrap()),
         ("getacl", Some(s)) => auth.get_acl(s.value_of("NAME").unwrap()),
-        ("setacl", Some(s)) => auth.set_acl(s.value_of("NAME").unwrap(), s.value_of("USER").unwrap(), s.value_of("ACL").unwrap()),
+        ("setacl", Some(s)) => auth.set_acl(
+            s.value_of("NAME").unwrap(),
+            s.value_of("USER").unwrap(),
+            s.value_of("ACL").unwrap(),
+        ),
         (_, _) => panic!(),
     }
 }
 
-fn sshkey_sub_cmd(args: Option<&ArgMatches>, auth: &Blih) -> Result<String, BlihErr>
-{
+fn sshkey_sub_cmd(args: Option<&ArgMatches>, auth: &Blih) -> Result<String, BlihErr> {
     let args = match args {
         Some(s) => s,
-        None    => panic!(),
+        None => panic!(),
     };
     match args.subcommand() {
-        ("list", Some(_))   => auth.list_key(),
+        ("list", Some(_)) => auth.list_key(),
         ("upload", Some(s)) => auth.upload_key_path(s.value_of("PATH").unwrap()),
         (_, _) => panic!(),
     }
